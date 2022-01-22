8b4edde3b7f25cca3ec773fdc8ae68e7804bd770
Isabel Maskrey <isabel@satyrn.dev>
2022-01-22 12:18:33 -0500

    🚧 [WIP} Update SowWhatYouReap.java
    Continued work on main plugin file.

--- src/main/java/dev/satyrn/sowwhatyoureap/SowWhatYouReap.java
+++ src/main/java/dev/satyrn/sowwhatyoureap/SowWhatYouReap.java
@@ -1,18 +1,34 @@
 package dev.satyrn.sowwhatyoureap;
 
 import dev.satyrn.sowwhatyoureap.configuration.Configuration;
+import dev.satyrn.sowwhatyoureap.events.SowWhatYouReapEvents;
 import dev.satyrn.sowwhatyoureap.lang.I18n;
 import net.milkbowl.vault.permission.Permission;
+import org.bstats.bukkit.Metrics;
+import org.bstats.charts.SimplePie;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Locale;
 import java.util.logging.Level;
 
+/**
+ * Manages the lifecycle of the Sow What You Reap plugin.
+ *
+ * @author Isabel Maskrey
+ * @since 0.0.0-SNAPSHOT
+ */
 public final class SowWhatYouReap extends JavaPlugin {
+    // The internationalization provider instance.
     private @Nullable I18n i18n;
 
+    /**
+     * Handles the plugin enable lifecycle event.
+     *
+     * @since 0.0.0-SNAPSHOT
+     */
     @Override
     public void onEnable() {
         // Cancel load if Vault is not present.
@@ -46,19 +62,53 @@ public final class SowWhatYouReap extends JavaPlugin {
         }
         final @NotNull Permission permission = registeredServiceProvider.getProvider();
 
+        // Register events and permissions.
+        this.registerEvents(permission, configuration);
+        this.registerCommands(permission, configuration);
 
+        // Initialize metrics
+        if (configuration.metrics.value()) {
+            final Metrics metrics = new Metrics(this, 14022);
+            metrics.addCustomChart(new SimplePie("require_hoe", configuration.requireHoe.value()::toString));
+            metrics.addCustomChart(new SimplePie("damage_hoe", configuration.damageHoe.value()::toString));
+            metrics.addCustomChart(new SimplePie("apply_fortune", configuration.applyFortune.value()::toString));
+            metrics.addCustomChart(new SimplePie("require_seeds_to_replant", configuration.requireSeedsToReplant.value()::toString));
+            metrics.addCustomChart(new SimplePie("hand_yield", () -> configuration.handYield.value().toString().toLowerCase(Locale.ROOT)));
+            metrics.addCustomChart(new SimplePie("locale", configuration.locale::value));
+        }
     }
 
+    /**
+     * Handles the plugin disable lifecycle event.
+     *
+     * @since 0.0.0-SNAPSHOT
+     */
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        if (this.getServer().isStopping()) {
+            this.getLogger().log(Level.INFO, "Server shutting down. Thanks for using Sow What You Reap!");
+        } else {
+            this.getLogger().log(Level.WARNING, "Plugin disabled before server shutdown! Either you are using a plugin manager or a dependency failed to load! See log for more details.");
+        }
+
+        if (this.i18n != null) {
+            this.i18n.disable();
+        }
     }
 
-    private void registerEvents(final Permission permission) {
-
+    /**
+     * Registers events for the plugin.
+     *
+     * @param permission The permission manager.
+     * @param configuration The configuration instance.
+     * @since 0.0.0-SNAPSHOT
+     */
+    private void registerEvents(final Permission permission, final Configuration configuration) {
+        this.getServer().getPluginManager().registerEvents(new SowWhatYouReapEvents(this, permission, configuration), this);
     }
 
-    private void registerCommands(final Permission permission) {
+    private void registerCommands(final Permission permission, final Configuration configuration) {
 
     }
 }
