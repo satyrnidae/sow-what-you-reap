afb3a65bf92001bd985036be95c743688875b624
Isabel Maskrey <isabel@satyrn.dev>
2022-01-22 12:18:04 -0500

    🚧 [WIP] Update SowWhatYouReapEvents.java
    Continued work on events

--- src/main/java/dev/satyrn/sowwhatyoureap/events/SowWhatYouReapEvents.java
+++ src/main/java/dev/satyrn/sowwhatyoureap/events/SowWhatYouReapEvents.java
@@ -4,7 +4,8 @@ import dev.satyrn.papermc.api.configuration.v4.ConfigurationConsumer;
 import dev.satyrn.sowwhatyoureap.configuration.Configuration;
 import dev.satyrn.sowwhatyoureap.configuration.ConfigurationRegistry;
 import dev.satyrn.sowwhatyoureap.utils.HandYields;
-import jdk.tools.jlink.plugin.Plugin;
+import net.milkbowl.vault.permission.Permission;
+import org.bukkit.CropState;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,13 +13,14 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 public class SowWhatYouReapEvents implements Listener, ConfigurationConsumer<Configuration> {
-
     private final @NotNull Plugin plugin;
+    private final @NotNull Permission permission;
 
     private List<Material> hoeItems;
 
@@ -49,8 +51,9 @@ public class SowWhatYouReapEvents implements Listener, ConfigurationConsumer<Con
     private int netherWartMinYield;
     private int netherWartMaxYield;
 
-    public SowWhatYouReapEvents(final @NotNull Plugin plugin, final @NotNull Configuration configuration) {
+    public SowWhatYouReapEvents(final @NotNull Plugin plugin, final @NotNull Permission permission, final @NotNull Configuration configuration) {
         this.plugin = plugin;
+        this.permission = permission;
         this.reloadConfiguration(configuration);
         ConfigurationRegistry.registerConsumer(this);
     }
@@ -83,7 +86,7 @@ public class SowWhatYouReapEvents implements Listener, ConfigurationConsumer<Con
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
-        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK || event.getClickedBlock() == null) {
             return;
         }
         // Event 1 fires for main hand item
@@ -91,13 +94,69 @@ public class SowWhatYouReapEvents implements Listener, ConfigurationConsumer<Con
 
         if (event.getHand() == EquipmentSlot.OFF_HAND) {
             // Offhand usage
+            final ItemStack offhandItem = event.getPlayer().getInventory().getItemInOffHand();
+            if (!this.isItemHoe(offhandItem)) {
+                // Don't try to harvest with a non-hoe in our offhand.
+                return;
+            }
+            // Ensure main hand shouldn't be used instead.
+            final ItemStack mainHandItem = event.getPlayer().getInventory().getItemInMainHand();
+            if (!this.isItemHoe(mainHandItem)) {
+                // Only process hoes in offhand if main hand is not already a hoe.
+                this.tryToHarvestBlock(event.getClickedBlock().getType(), event, false);
+            }
         } else {
-            // onhand usage
+            final ItemStack mainHandItem = event.getPlayer().getInventory().getItemInMainHand();
+            if (!this.isItemHoe(mainHandItem)) {
+                // We are using a non-hoe in our main hand, so make sure we aren't using a hoe in our offhand
+                final ItemStack offhandItem = event.getPlayer().getInventory().getItemInOffHand();
+                if (this.isItemHoe(offhandItem)) {
+                    // We have an offhand hoe, use that instead.
+                    return;
+                }
+                tryToHarvestBlock(event.getClickedBlock().getType(), event, true);
+            } else {
+                tryToHarvestBlock(event.getClickedBlock().getType(), event, false);
+            }
         }
     }
 
+    private void harvestBeetroots(@NotNull PlayerInteractEvent event, boolean isHand) {
+
+    }
+
+    private void harvestCarrots(@NotNull PlayerInteractEvent event, boolean isHand) {
+
+    }
+
+    private void harvestNetherWart(@NotNull PlayerInteractEvent event, boolean isHand) {
+
+    }
+
+    private void harvestPotatoes(@NotNull PlayerInteractEvent event, boolean isHand) {
+
+    }
+
+    private void harvestWheat(@NotNull PlayerInteractEvent event, boolean isHand) {
+
+    }
 
     private boolean isItemHoe(final @NotNull ItemStack item) {
-        return this.hoeItems.stream().anyMatch(hoeItem -> item.getType() == hoeItem);
+        return item.getAmount() > 0 && !item.getType().isEmpty() && this.hoeItems.stream().anyMatch(hoeItem -> item.getType() == hoeItem);
+    }
+
+    private void tryToHarvestBlock(@NotNull Material blockType, @NotNull PlayerInteractEvent event, boolean isHand) {
+        if (isHand && this.requireHoe) {
+            // We cannot harvest with our hand when requireHoe is enabled so don't even try.
+            return;
+        }
+
+        switch (blockType) {
+            case BEETROOTS -> harvestBeetroots(event, isHand);
+            case CARROTS -> harvestCarrots(event, isHand);
+            case NETHER_WART -> harvestNetherWart(event, isHand);
+            case POTATOES -> harvestPotatoes(event, isHand);
+            case WHEAT -> harvestWheat(event, isHand);
+        }
     }
 }
